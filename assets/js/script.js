6ab630d0b027e0fa93d9243b7de0d90a259d8369
bkfleet1 <bkfleet1@gmail.com>
2021-12-07 14:12:19 -0600

    updated js to separate input event and data for task-item and task-info

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,39 @@ var formEl = document.querySelector("#task-form");
 //List items
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function(event) { 
-  /* prevents browser default behavior, which is to refresh */
-  event.preventDefault(); 
 
-  /*use square brackets [ ] in a selector, we're trying to select an HTML element by one of its attributes. In this case, we're selecting the <input> element on the page that has a name attribute set to a value of "task-name". */
-  var taskNameInput = document.querySelector("input[name='task-name']").value;
-  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+var taskFormHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // package up data as an object
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+  }
+
+
+
+var createTaskEl = function(taskDataObj) {
  // create list item
     var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
+    listItemEl.className = "task-item";
 
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
-        taskInfoEl.className = "task-info";
+    taskInfoEl.className = "task-info";
 
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
-  }; 
+  }
 
-  formEl.addEventListener("submit", createTaskHandler);
-
-  
+formEl.addEventListener("submit", taskFormHandler);
